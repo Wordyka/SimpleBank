f1c66074a764b06a442b84205bbebeb0a43e151c
wordykanainggolan1212@gmail.com <77338737+Wordyka@users.noreply.github.com>
2024-04-23 19:23:47 +0700

    Login user API that return JWT or PASETO token

--- api/account_test.go
+++ api/account_test.go
@@ -95,7 +95,7 @@ func TestGetAccountAPI(t *testing.T) {
 			//build stubs
 
 			//start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
@@ -112,10 +112,10 @@ func TestGetAccountAPI(t *testing.T) {
 
 }
 
-func randomAccount() db.Account {
+func randomAccount(owner string) db.Account {
 	return db.Account{
 		ID:       util.RandomInt(1, 1000),
-		Owner:    util.RandomOwner(),
+		Owner:    owner,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
